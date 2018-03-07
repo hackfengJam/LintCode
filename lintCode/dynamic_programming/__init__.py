ce7523d7c3572283683ec47157dfc1fa82a0d50c
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2018-03-07 18:07:56 +0800

    update

---
+++ lintCode/dynamic_programming/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__Author__ = "HackFun"
+__Date__ = "2018/3/7 下午5:58"
---
+++ lintCode/dynamic_programming/lintCode_92_pending.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__Author__ = "HackFun"
+__Date__ = "2018/3/7 下午5:56"
+
+
+class Solution(object):
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @return: The maximum size
+    """
+
+    def backPack(self, m, A):
+        # write your code here
+        pass
